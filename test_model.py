7a74c9e8732b6ed3661254018ca6e80c8182ef76
Auro Tripathy <ipserv@yahoo.com>
2019-10-17 22:10:38 -0700

    initial

---
+++ test_model.py
@@ -0,0 +1,17 @@
+import torch
+
+def test_model(model, dataloaders):
+
+    model.eval()
+    running_corrects = 0
+    with torch.no_grad():
+        for _, (inputs, labels) in enumerate(dataloaders['test']):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outputs = model(inputs)
+            _, predictions = torch.max(outputs, 1)  # predictions == argmax
+            running_corrects += torch.sum(predictions == labels.data)
+            acc = running_corrects.double() / dataset_sizes['test']
+        logging.info('Test accuracy: {:.4f}'.format(acc))
+
