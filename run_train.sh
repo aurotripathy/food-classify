2bb9678f676009ff0edb4dcbe3c86752159e387b
aurotripathy <ipserv@yahoo.com>
2019-10-20 16:11:32 -0700

    Update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -1,4 +1,5 @@
-# for wide resent101, large model BS is much smaller, otherwise 1024
+# for wide resnet101, due to the model being large the BS is much smaller; 
+# for smaller models BS can be 1024 for 32GB GPU machines
 python3 train_validate.py --batch-size 128 \
        --epochs 100 \
        --train-data ./../../data/food-101/food-101/train_val_test/
