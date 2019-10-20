7fd64dfe05e896614fcf3a868f7d488d2e00a92e
Auro Tripathy <ipserv@yahoo.com>
2019-10-20 15:13:47 -0700

    using wide_resnet

--- run_train.sh
+++ run_train.sh
@@ -1,4 +1,5 @@
-python3 train_validate.py --batch-size 1024 \
+# for wide resent101, large model BS is much smaller, otherwise 1024
+python3 train_validate.py --batch-size 128 \
        --epochs 100 \
        --train-data ./../../data/food-101/food-101/train_val_test/
 
