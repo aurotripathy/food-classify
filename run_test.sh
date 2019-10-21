abe1225f251856b1e304053bfdc2a926f510df19
Auro Tripathy <ipserv@yahoo.com>
2019-10-21 11:02:21 -0700

    improvements to the command line

--- run_test.sh
+++ run_test.sh
@@ -1,1 +1,1 @@
-python test_model.py --model-file ./trained_models/checkpoint.pth --train-data /media/auro/RAID\ 5/food-101/food-101/train_val_test/
+python test_model.py --model-file ./trained_models/checkpoint.pth --test-data /media/auro/RAID\ 5/food-101/food-101/train_val_test/
