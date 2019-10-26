e83e4a70bb6e80486011630d507e8b18709f8a81
Auro Tripathy <ipserv@yahoo.com>
2019-10-26 00:38:01 -0700

    init

--- run_train.sh
+++ run_train.sh
@@ -1,6 +1,9 @@
 # for wide resnet101, due to the model being large the BS is much smaller; 
 # for smaller models BS can be 1024 for 32GB GPU machines
-python3 train_validate.py --batch-size 128 \
-       --epochs 100 \
-       --train-data ./../../data/food-101/food-101/train_val_test/
 
+# python3 train_validate.py --batch-size 128 \
+#        --epochs 100 \
+#        --train-data ./../../data/food-101/food-101/train_val_test/
+
+# for use in the smaller system
+python3 train_validate.py --batch-size 72 --epochs 100  --train-data /media/auro/RAID\ 5/food-101/food-101
